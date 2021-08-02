fc9db3bf41bf65651807773fe3b2be207fc417a3
Pranav Vignesh Sreedhar <53910832+Pranav-Vignesh@users.noreply.github.com>
2021-08-02 11:24:15 +0200

    Update IR_Sensor_MQTT.ino

--- IR_Sensor_MQTT/IR_Sensor_MQTT.ino
+++ IR_Sensor_MQTT/IR_Sensor_MQTT.ino
@@ -3,9 +3,9 @@
 
 // Update these with values suitable for your network.
 
-const char* ssid = "Livebox-F7A6";
-const char* password = "6D5CF624D45C4DEAD5514FA436";
-const char* mqtt_server = "192.168.1.10";
+const char* ssid = "WIFI";
+const char* password = "WIFI PASSWORD";
+const char* mqtt_server = "IP ADDRESS OF MQTT BROKER";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
